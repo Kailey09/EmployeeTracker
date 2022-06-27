699b958aaa7568411816c4c03f92e3aea78bf080
Kailey09 <kcoop1323@gmail.com>
2022-06-27 15:02:05 -0400

    added connection to get role

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
-const mysql = require('mysql');
-const inquier = require('inquirer');
-const consoleTables = require('console.table');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const consoleTables = require("console.table");
 var roles = [];
 var employees = [];
 
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    port: 3001,
+    user: "root",
+    password: "Nalabean13",
+    database: "employeesDB"
+});
+
+// get Role
+const getRole = () => {
+    connection.query(`SELECT title, role_id FROM role`, (err,res) => {
+        if (err) throw err;
+        roles =[];
+        for (let i = 0; i < res.length; i++) {
+            const id = res[i].role_id;
+            const title = res[i].title;
+            var newRole ={
+                name: title,
+                value: id
+            }
+            roles.push(newRole)
+        }
+        return roles;
+    });
+};
